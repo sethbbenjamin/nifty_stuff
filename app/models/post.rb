1543e3aa53c197c2fd5a87ff5de8ee574c1279c4
sethbbenjamin <sethbbenjamin@gmail.com>
2015-10-01 13:17:33 -0600

    Add CRUD features

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true, length: { minimum: 2 }
+  validates :description, presence: true
+  validates :link, presence: true
+  #validates :link, presence: true, :format => URI::regexp(%w(http https))
 end
