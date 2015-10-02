3a5d783421a6ace7a0ada44934cd83d63ee9de6b
sethbbenjamin <sethbbenjamin@gmail.com>
2015-10-02 11:39:45 -0600

    Finish admin authentication

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ActiveRecord::Base
+
+  belongs_to :user
+
+
   validates :title, presence: true, length: { minimum: 2 }
   validates :description, presence: true
   validates :link, presence: true
